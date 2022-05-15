fd2957ee0c564929bbdf6c479e01bb59e8594ddb
William Astle <lost@l-w.ca>
2022-05-14 21:30:59 -0600

    Update raw output to work with RMB only definitions at the start
    It seems useful to be able to define a number of symbols at the start of the
    source file without generating a run of NUL output. This update adjusts the
    output to ignore any number of statements that generate no output as long as
    there is a subsequent ORG statement. If there is no ORG statement after
    them, the NUL sequence should still appear.

--- lwasm/output.c
+++ lwasm/output.c
@@ -32,6 +32,7 @@ Contains the code for actually outputting the assembled code
 #include <lw_expr.h>
 
 #include "lwasm.h"
+#include "instab.h"
 
 void write_code_raw(asmstate_t *as, FILE *of);
 void write_code_decb(asmstate_t *as, FILE *of);
@@ -245,12 +246,24 @@ void write_code_rawrel(asmstate_t *as, FILE *of)
 raw merely writes all the bytes directly to the file as is. ORG is just a
 reference for the assembler to handle absolute references. Multiple ORG
 statements will produce mostly useless results
+
+However, if a run of RMBs exists at the start that is ended by an ORG
+statement, that run of RMBs will not be output as a zero fill.
 */
 void write_code_raw(asmstate_t *as, FILE *of)
 {
 	line_t *cl;
+	line_t *sl;
 	
-	for (cl = as -> line_head; cl; cl = cl -> next)
+	sl = as -> line_head;
+	for (cl = sl; cl; cl = cl -> next)
+	{
+		if (cl -> outputl > 0)
+			break;
+		if (instab[cl -> insn].flags & lwasm_insn_org)
+			sl = cl;
+	}
+	for (cl = sl; cl; cl = cl -> next)
 	{
 		if (cl -> len > 0 && cl -> outputl < 0)
 		{
